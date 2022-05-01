70b913295795083ea2e22b104603ecf0a40e5441
Stephen <ck19955@bristol.ac.uk>
2022-05-01 14:20:04 +0100

    Added comments and made the file into a function that can be used in future files

--- user_interface.py
+++ user_interface.py
@@ -3,42 +3,76 @@ import numpy as np
 import random
 from expressionGenerator import polynomial
 
-# Set up question
-difficulty = 3
-q1 = polynomial(difficulty)
-solution_1 = integrate(q1)
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-number_of_choice = 4
-if number_of_choice > 9:
-    raise ValueError("Number of choices is too large")
-answers = [0]*number_of_choice
-answers[0] = solution_1
-answer_letters = [' ']*number_of_choice
-answer_letters[0] = 'A'
-for i in range(1, number_of_choice):
-    solution = integrate(polynomial(difficulty))
-    answers[i] = solution
-    answer_letters[i] = alphabet[i]
 
-random.shuffle(answers)
-print("Question: \n")
-pprint(q1, use_unicode=True)
-print(" \n==========================================")
-print("\nSolutions: ")
+def multi_choice(topic, difficulty, choices):
+    """
+    Gives a multiple choice question
+
+    :param topic: function
+            What area of maths wants to be covered. (I.e polynomials, trigonometric, etc)
+    :param difficulty: integer
+            Difficulty for the area of maths. Range (1-3)
+    :param choices: integer
+            Number of choices in the multiple choice
+    :return:
+    """
+
+    # Set up question
+    question = topic(difficulty)
+    correct_solution = integrate(question)
+
+    # Set up letters for choices
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+
+    # Can't have too many options
+    if choices > 9:
+        raise ValueError("Number of choices is too large")
+
+    # Set up lists for solution collection
+    answers = [0]*choices
+    answers[0] = correct_solution
+    answer_letters = [' ']*choices
+    answer_letters[0] = 'A'
+
+    # Create dummy solutions from same pool of questions with same difficulty
+    for i in range(1, choices):
+        solution = integrate(topic(difficulty))
+        answers[i] = solution
+        answer_letters[i] = alphabet[i]
+
+    # Shuffle the solution and dummy solutions
+    random.shuffle(answers)
+
+    # Print out questions and solution choices
+    print("Question: \n")
+    pprint(question, use_unicode=True)
+    print(" \n==========================================")
+    print("\nSolutions: ")
+
+    for i in range(len(answers)):
+        print('Answer ' + str(answer_letters[i]) + ': ')
+        pprint(answers[i], use_unicode=True)
+        print("\n")
+
+    # Get user's answer
+    valid_answer = False
+    user_answer = ''
+    while not valid_answer:
+        user_answer = input('What is the integral to the question? (type in the letter of the answer)')
+        if user_answer in answer_letters:
+            valid_answer = True
+        else:
+            print('Your input is not a letter of one of the choices')
+    answer_index = answer_letters.index(user_answer)
+    selected_answer = answers[answer_index]
+
+    if selected_answer == correct_solution:
+        print('correct!')
+    else:
+        print('Incorrect :(')
+        print('The correct answer was: ' + str(correct_solution))
 
 
-for i in range(len(answers)):
-    print('Answer ' + str(answer_letters[i]) + ': ')
-    pprint(answers[i], use_unicode=True)
-    print("\n")
-
-user_answer = input('Is the solution A, B, C or D?')
-answer_index = answer_letters.index(user_answer)
-selected_answer = answers[answer_index]
-
-if selected_answer == solution_1:
-    print('correct!')
-else:
-    print('Incorrect :(')
-    print('The correct answer was: ' + str(solution_1))
-
+if __name__ == '__main__':
+    # Example of a multiple choice for polynomials of difficulty 3 and there being 4 choices
+    multi_choice(polynomial, 3, 4)
