b08c075ec84e6517c3830b5b01fe48c1e5f4599f
TheoLeVay1 <xh19142@bristol.ac.uk>
2022-05-03 13:39:33 +0100

    Update ELO_global.py

--- ELO_global.py
+++ ELO_global.py
@@ -48,10 +48,11 @@ def simulate_elo(games, learning_rate = 0.1):
     """
     # The predicted elo ratings of the students
     starting_rating = 500
+
     student_rating = {'student1': starting_rating, 'student2': starting_rating,
     'student2': starting_rating, 'student3': starting_rating, 'student4': starting_rating,
-    'student5': starting_rating, 'student6': starting_rating, 'student7': starting_rating,
-    'student8': starting_rating, 'student9': starting_rating, 'student10': starting_rating}
+    'student5': starting_rating}#, 'student6': starting_rating, 'student7': starting_rating,
+    # 'student8': starting_rating, 'student9': starting_rating, 'student10': starting_rating}
 
     # Elo ratings for questions
 
@@ -70,71 +71,83 @@ def simulate_elo(games, learning_rate = 0.1):
     'Blueprint 12':1100,'Blueprint 13':1150, 'Blueprint 14':1200, 'Blueprint 15':1350,
     'Blueprint 16':1400, 'Blueprint 17':1500}
 
-    question_probability = {'Blueprint 1.1': 0.9 , 'Blueprint 1.2': 0.85, 'Blueprint 1.3': 0.82,
-    'Blueprint 2': 0.8, 'Blueprint 3': 0.75, 'Blueprint 4': 0.72,
-    'Blueprint 5': 0.65, 'Blueprint 6': 0.68, 'Blueprint 7.1': 0.65,
-    'Blueprint 7.2': 0.62, 'Blueprint 7.3': 0.6, 'Blueprint 7.4': 0.56,
-    'Blueprint 8': 0.52, 'Blueprint 9': 0.5, 'Blueprint 10.1': 0.45,
-    'Blueprint 10.2': 0.54, 'Blueprint 10.3': 0.52, 'Blueprint 11.1': 0.5,
-    'Blueprint 11.2': 0.475, 'Blueprint 11.3': 0.45, 'Blueprint 11.4': 0.25,
-    'Blueprint 11.5': 0.225, 'Blueprint 12': 0.2, 'Blueprint 13': 0.175,
-    'Blueprint 14': 0.15, 'Blueprint 15': 0.05, 'Blueprint 16': 0.08,
-    'Blueprint 17': 0.05}
+    question_probability = {'Blueprint 1.1': 0.95 , 'Blueprint 1.2': 0.9, 'Blueprint 1.3': 0.85,
+    'Blueprint 2': 0.825, 'Blueprint 3': 0.8, 'Blueprint 4': 0.775,
+    'Blueprint 5': 0.75, 'Blueprint 6': 0.72, 'Blueprint 7.1': 0.7,
+    'Blueprint 7.2': 0.65, 'Blueprint 7.3': 0.6, 'Blueprint 7.4': 0.82,
+    'Blueprint 8': 0.8, 'Blueprint 9': 0.78, 'Blueprint 10.1': 0.76,
+    'Blueprint 10.2': 0.74, 'Blueprint 10.3': 0.72, 'Blueprint 11.1': 0.7,
+    'Blueprint 11.2': 0.69, 'Blueprint 11.3': 0.675, 'Blueprint 11.4': 0.65,
+    'Blueprint 11.5': 0.625, 'Blueprint 12': 0.6, 'Blueprint 13': 0.58,
+    'Blueprint 14': 0.575, 'Blueprint 15': 0.55, 'Blueprint 16': 0.525,
+    'Blueprint 17': 0.5}
 
     # List of keys for dictionaries
     student_keys = list(student_rating.keys())
     question_keys = list(question_ratings.keys())
 
     # Set up a list for data
-    player_data = np.empty(games)
+    player_data = np.empty([len(student_keys), games])
     player_data[:, 0] = starting_rating
 
     # question_data = [0]*games
-    question_data = np.zeros(shape = (games, len(question_ratings)))
-    question_data[0] = list(question_ratings.values())
+    question_data = np.empty([len(question_keys), games])
+    question_data[:, 0] = list(question_ratings.values())
 
     for i in range(1, games):
+        if i < 50:
+            scale = 500
+        if 50 < i < 100:
+            scale = 250
         if i < 100:  # k value varies depending on the number of games played
             k = 32
         elif 100 <= i < 500:
-            k = 32
+            k = 16
+            scale = 200
         else:
             k = 10
+            scale = 100
 
         # Initialise the player rating
         # player = student_keys[0]
         for student in student_keys:
             player_rating = student_rating[student]
+            idx = student_keys.index(student)
 
             sample_list = list(question_keys)  # Create a list to sample from without replacement
 
             # Use normal distribution of player rating to select a number, and then
             # choose question with closest score to that number
-            varied_value = np.random.normal(loc = player_rating, scale = 200)
+            varied_value = np.random.normal(loc = player_rating, scale = scale)
 
             # Selects question from question bank with ELO score closest to value
             # selected from distribution
 
             question, question_rating = min(question_ratings.items(), key = lambda x: abs(varied_value - x[1]))
-            p_win = question_probability[question]
+
+            # Require varying abilities --> linspace between different ranges by students
+
+            p_win = question_probability[question] - 0.1 * idx
 
             # Calculate new ratings and assign the new values into the dictionary
             new_ratings = update_ratings(player_rating, question_rating, p_win, k)
-            student_rating['student'], question_ratings[question], outcome = new_ratings
+            student_rating[student], question_ratings[question], outcome = new_ratings
 
             # Fill in the new data point for the ratings of each player
-            player_data[i] = student_rating['student']
-            question_data[i] = list(question_ratings.values())
+        player_data[:, i] = list(student_rating.values())
+        question_data[:, i] = list(question_ratings.values())
+    # print(question_data)
 
     # Plot data
-    plt.plot(player_data, 'k')#, linewidth = 2)
-    plt.plot(question_data)#, linewidth = 2)
-    plt.legend(['Player'], loc = 1)
-    # plt.title('Example student ELO graph')
+    plt.figure()
+    for i in range(player_data.shape[0]):
+        plt.plot(player_data[i], 'k', linewidth = 1)
+    for i in range(question_data.shape[0]):
+        plt.plot(question_data[i], linewidth = 0.5)
+
     plt.xlabel('Number of questions answered')
     plt.ylabel('Elo score')
-    print(player_data)
-    plt.show()
-    # plt.savefig('with_learning.svg', bbox_inches='tight')
 
-simulate_elo(200, learning_rate = 0.01)
+    plt.show()
+
+simulate_elo(500, learning_rate = 0.01)
